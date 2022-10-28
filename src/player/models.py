dd2e91756d060df5deb0b2d77c96982b6277e9b0
hailatGH <hailat.alx@gmail.com>
2022-10-28 09:45:19 +0300

    local changes

--- src/player/models.py
+++ src/player/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from core import validators
+from core.compress_images import CompressedImageField
 
 def Station_Cover_Images(instance, filename):
     return '/'.join(['Media_Files', 'Station_Cover_Images', str(instance.station_name) + "_" + str(filename)])
@@ -13,7 +14,8 @@ class RadioStationModel(models.Model):
     station_name = models.CharField(null=False, blank=True, unique=True, max_length=256)
     station_frequency = models.FloatField(null=False, blank=False, unique=True)
     station_url = models.CharField(null=False , blank=True, max_length=1023)
-    station_cover = models.ImageField(upload_to=Station_Cover_Images, validators=[validators.validate_image_extension], null=False, blank=True)
+    station_cover = CompressedImageField(upload_to=Station_Cover_Images, validators=[validators.validate_image_extension], null=False, blank=True, quality=1)
+    # station_cover = models.ImageField(upload_to=Station_Cover_Images, validators=[validators.validate_image_extension], null=False, blank=True)
     station_description = models.TextField(blank=True, null=True, max_length=1023)
     encoder_FUI =  models.CharField(null=False, blank=True, max_length=1023)
     created_at = models.DateTimeField(auto_now_add=True)
