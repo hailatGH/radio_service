746e93ae066eae4c9618d308e98de7b3e973b0bb
hailatGH <hailat.alx@gmail.com>
2022-12-15 16:14:01 +0300

    done

--- src/core/urls.py
+++ src/core/urls.py
@@ -39,7 +39,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('webApp/', include(webApprouter.urls)),
     path('mobileApp/', include(mobileApprouter.urls)),
-    path('openapi.yml', schema_view.without_ui(
+    path('openapi.json', schema_view.without_ui(
         cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger',
          cache_timeout=0), name='schema-swagger-ui'),
