ec2d1530b3a5398a3a804f09248fd124fdd8f848
hailatGH <hailat.alx@gmail.com>
2022-12-15 14:16:03 +0300

    openapi endpoint set

--- src/core/urls.py
+++ src/core/urls.py
@@ -23,23 +23,26 @@ from drf_yasg import openapi
 from .router import *
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Radio Service API",
-      default_version='v1',
-      description="Radio Service API description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="admin@kinideas.com"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
+    openapi.Info(
+        title="Radio Service API",
+        default_version='v1',
+        description="Radio Service API description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="admin@kinideas.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('webApp/', include(webApprouter.urls)),
     path('mobileApp/', include(mobileApprouter.urls)),
-    path('OpenAPI_Config_Radio_Service.yaml', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
+    path('openapi.yml', schema_view.without_ui(
+        cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
